4a735c2b54c3583ea0aa18390777b107241ac147
vitorgabrielgalera <vitor.ggabrielgalera@gmail.com>
2024-07-15 21:46:28 -0300

    início da tela de login

---
+++ app_loja.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+class TelaLogin(Screen):
+    pass
+
+class GerenciadorTelas(ScreenManager):
+    pass
+
+kv = Builder.load_file('loja.kv')
+
+class LojaApp(App):
+    def build(self):
+        return kv
+    
+if __name__ == "__main__":
+    LojaApp().run()
